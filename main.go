303e81691fed299e9f0ce075d5d18ef4913f28d1
jeanroths <jean.machado@sou.inteli.edu.br>
2024-03-22 10:15:42 -0300

    teste AWS Cognito - OAuth

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	  "fmt"
+	  "os"
+	  "github.com/gin-gonic/gin"
+	  "github.com/joho/godotenv"
+	  congnitoClient "cognitoClient"
+	  "errors"
+	  "net/http"
+	  "json"
+)
+
+func CreateUser(c *gin.Context, cognito congnitoClient.CognitoInterface) error {
+    var user congnitoClient.User
+    if err := c.ShouldBindJSON(&user); err != nil {
+      return errors.New("invalid json")
+    }
+    err := cognito.SignUp(&user)
+    if err != nil {
+      return errors.New("could not create use")
+    }
+    return nil
+  }
+
+func main() {
+    err := godotenv.Load()
+    if err != nil {
+      panic(err)
+    }
+    cognitoClient := congnitoClient.NewCognitoClient(os.Getenv("COGNITO_CLIENT_ID"))
+    r := gin.Default()
+
+	r.POST("user", func(context *gin.Context) {
+        err := CreateUser(context, cognitoClient)
+        if err != nil {
+            context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+            return
+        }
+        context.JSON(http.StatusCreated, gin.H{"message": "user created"})
+    })
+
+
+    fmt.Println("Server is running on port 8080")
+    err = r.Run(":8080")
+    if err != nil {
+      panic(err)
+    }
+  }
